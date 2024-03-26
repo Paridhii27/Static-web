95cc43632a25b0fb391646aa6efe8d9654edf310
Paridhi <paridhigarg27@gmail.com>
2024-03-26 02:13:38 -0400

    Adding js

--- script.js
+++ script.js
@@ -1,50 +1,11 @@
-// let screen;
-// let glitchShader;
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("canvasContainer");
+  background(0);
+}
 
-// function preload() {
-//   glitchShader = loadShader("shader.vert", "shader.frag");
-// }
-
-// function setup() {
-//   createCanvas(600, 600, WEBGL);
-//   screen = createGraphics(width, height);
-
-//   screen.background(50);
-//   screen.stroke(255);
-//   screen.strokeWeight(5);
-
-//   shader(glitchShader);
-// }
-
-// function draw() {
-//   if (mouseIsPressed) {
-//     screen.line(mouseX, mouseY, pmouseX, pmouseY);
-//   }
-
-//   drawScreen();
-// }
-
-// function drawScreen() {
-//   glitchShader.setUniform("texture", screen);
-//   glitchShader.setUniform("noise", getNoiseValue());
-
-//   rect(-width / 2, -height / 2, width, height);
-// }
-
-// function getNoiseValue() {
-//   let v = noise(millis() / 100);
-//   const cutOff = 0.5;
-
-//   if (v < cutOff) {
-//     return 0;
-//   }
-
-//   v = pow(((v - cutOff) * 1) / (1 - cutOff), 2);
-
-//   return v;
-// }
-
-// function windowResized() {
-//   // Resize the canvas when the window is resized
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+function draw() {
+  noStroke();
+  fill(mouseX, 150);
+  ellipse(mouseX, mouseY, mouseX, 5);
+}
